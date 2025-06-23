e28a6c21fc02358a8d5f0e76fb7508a85b4e5253
sora <147121795+sora7716@users.noreply.github.com>
2025-06-23 11:30:00 +0900

    拡縮をif文を使用せずに書いた

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 
 const char kWindowTitle[] = "GC1D 01 イイヅカ　ソラ";
 
+//ベクトルの長さ
+const float kVectorMax = 2.0f;//最大値
+const float kVectorMin = 0.5f;//最低値
+
 //2次元のベクトル
 typedef struct Vector2 {
 	float x;
@@ -78,12 +82,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		//スケール加算
-		scale += scaleSpeed;
-
+		scaleSpeed += 1.0f / 240.0f;
 		//0.5～2.0の範囲に収める
-		if (scale <= 0.5f || scale > 2.0f) {
-			scaleSpeed *= -1;
-		}
+		scale = (sinf(2.0f * static_cast<float>(M_PI) * scaleSpeed) * 0.5f + 0.5f) * (kVectorMax - kVectorMin) + kVectorMin;
 
 		//回転
 		theta -= 1.0f / 120.0f * float(M_PI);
